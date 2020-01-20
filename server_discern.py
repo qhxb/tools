a9023c95734b4a054a0e9c00a9cf57aa5172e4ba
徐公子 <773478542@qq.com>
2020-01-20 15:07:07 +0800

    Update server_discern.py

--- server_discern.py
+++ server_discern.py
@@ -1,140 +1,174 @@
-ip = '127.0.0.1'
-port = 445
+# -*- coding: utf-8 -*-
+import multiprocessing
+import re
+import lib.feature as feature
+import threading
+import lib.discern as discern
+from IPy import IP
 
-# UDP端口探测、服务识别
-def UdpTftp():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(3)
+# 区间ip格式处理，ip2num、num2ip、gen_ip
+def ip2num(ip):
+    ips = [int(x) for x in ip.split('.')]
+    return ips[0] << 24 | ips[1] << 16 | ips[2] << 8 | ips[3]
+
+
+def num2ip(num):
+    return '%s.%s.%s.%s' % ((num >> 24) & 0xff, (num >> 16) & 0xff, (num >> 8) & 0xff, (num & 0xff))
+
+
+def gen_ip(ip):
+    start, end = [ip2num(x) for x in ip.split('-')]
+    return [num2ip(num) for num in range(start, end + 1) if num & 0xff]
+
+
+# 用户输入ip信息转生成器
+def getiplist(ipinput):
+    ipinput2 = []
     try:
-        sock.sendto(binascii.a2b_hex('000131006e6574617363696900'),(ip, port))#发送tftp特征数据
+        if ipinput:
+            if '-' in ipinput:
+                ipinput2 = gen_ip(ipinput)
+            else:
+                ips = IP(ipinput)  # 掩码ip网段地址格式处理
+                ipinput2 = [str(ip) for ip in ips]
     except Exception as e:
-        print e
-    while True:
-        data,ipport = sock.recvfrom(1024)
-        if data:
-            if 'File not found' in data:
-                print "IP：{ipport}\n发现UDP服务：{name}".format(ipport=ipport[0],port=port,name='TFTP')
-            break
+        return "输入ip信息{ipinput}不合法，请检查\n详情：{info}".format(ipinput=ipinput, info=str(e))
+    return ipinput2
 
-#TCP端口探测（TCP服务被动识别）
-def TcpPort():
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(3)
-        sock.connect((ip,port))
-        print '发现存活IP端口\nIP:{ip}、端口：{port}'.format(ip=ip,port=port)
-    except:
-        pass
-    #被动服务识别
-    try:
+
+class hostscan(object):
+    def __init__(self, ipinput='', blackip=[], tcpportlist=[], threadnum='', process_num=''):
+        self.__ip_input = getiplist(ipinput)  # 待扫描ip
+        self.__tcpport_list = tcpportlist  # 需要探测的TCP常见端口
+        self.__black_ip = blackip  # 黑名单ip池，比如127.0.0.1，如果部署在公网需要屏蔽内网ip
+        self.__process_num = process_num  # 进程数，每个进程去ip池顺序取IP扫描
+        self.__thread_num = threadnum  # 线程数，每个线程去端口池顺序取端口扫描
+
+        self.__tcp_feature = feature.TCP  # TCP识别特征库
+        self.__udp_feature = feature.UDP  # UDP识别特征库
+        self.__ping_timeout = 5  # ping超时时间
+        self.__ping_count = 1  # ping次数
+
+    # TCP扫描，步骤：TCP握手，握手成功的话识别是否有返回banner，如果有用feature.TCP去识别服务
+    def tcpscan(self, ip, hostname, portIterator, result):
         while True:
-            banner,ipport = sock.recv(1024)
-            print banner,ipport
-    except Exception as e:
-        print str(e)
+            try:
+                port = portIterator.next()
+            except:
+                return
+            if port:
+                tcp = discern.TcpPort(ip, port)  # TCP端口探测（TCP服务被动识别）
+                try:
+                    if tcp:
+                        if tcp['banner']:
+                            flag = 1   # 设置标志位，如果有banner但是没有识别出服务应该记住，用来后续更新我们的特征库
+                            for featuredetail in feature.TCP:
+                                list = featuredetail.split('|')
+                                reg = list[3].decode('string_escape')
+                                if list[2] == 'banner':
+                                    matchObj = re.search(reg, tcp['banner'], re.I | re.M)
+                                    if matchObj:
+                                        flag = 0
+                                        result.put(
+                                            "被动式发现TCP服务，IP：{},主机名：{}，端口：{},服务名称：{}，模式：banner".format(ip, hostname, port,
+                                                                                                     list[0]))
+                                        break
+                            if flag:
+                                result.put(
+                                    "发现TCP端口跟banner但是没有识别出服务，IP：{},主机名：{}，端口：{},banner：{}".format(ip, hostname, port,
+                                                                                                  tcp['banner']))
 
-#TCP主动服务识别
-def TcpServer():
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(3)
-        sock.connect((ip,port))
-        sock.send(binascii.a2b_hex('00000045ff534d4272000000001853c8000000000000000000000000fffffffe00000000002200024e54204c4d20302e31320002534d4220322e3030320002534d4220322e3f3f3f00'))#SMB2
-    except Exception as e:
-        print str(e)
-    try:
+                        else:
+                            for featuredetail in feature.TCP:
+                                tcpresult = discern.TcpServer(ip, port, featuredetail)
+                                try:
+                                    if tcpresult[1] == port:
+                                        result.put("主动式发现TCP服务，IP：{},主机名：{}，端口：{},服务名称：{}".format(ip, hostname, port,
+                                                                                                  tcpresult[2]))
+                                        # 默认识别成功时break，在端口复用的情况下，只能扫到其中一个服务，如果想都识别注释掉break
+                                        break
+                                    else:
+                                        list = featuredetail.split('|')
+                                        if list[2] == 'default' and list[1] == str(port):
+                                            result.put(
+                                                "被动式发现TCP服务，IP：{},主机名：{}，端口：{},服务名称：{}，模式：default".format(ip, hostname,
+                                                                                                          port,
+                                                                                                          list[0]))
+                                            break
+                                except:
+                                    pass
+                except Exception as e:
+                    # print str(e)
+                    pass
+    # UDP扫描，步骤：遍历UDP特征库，直接发包识别
+    def udpscan(self, ip, hostname, result):
+        for featuredetail in feature.UDP:
+            udpresult = discern.UdpPort(ip,featuredetail)
+            try:
+                if len(udpresult) == 3:
+                    result.put("主动式发现UDP服务，IP：{},主机名：{}，端口：{},服务名称：{}".format(udpresult[0], hostname, udpresult[1],udpresult[2]))
+            except:
+                pass
+
+    # 探测一个ip
+    def scanip(self, ipque, result):
         while True:
-            data = sock.recv(1024)
-            if data:
-                if 'SMB' in data:
-                    print "IP：{ipport}、端口：{port}\n发现TCP服务：{name}".format(ipport=ip,port=port, name='SMB')
-                break
-    except Exception as e:
-        print str(e)
+            try:
+                ip = ipque.get(False)
+            except Exception as e:
+                # print str(e)
+                return
+            if ip in self.__black_ip:  # 如果ip为黑名单ip，不扫描
+                pass
+            elif ip:
+                isip = discern.verbose_ping(ip, timeout=self.__ping_timeout, count=self.__ping_count)
+                if len(isip) == 3:
+                    try:
+                        hostname = discern.ip2hostname(ip)  # 步骤：主机存活探测/获取主机名——TCP端口探测/UDP服务识别——TCP服务识别
+                    except:
+                        hostname = ''
 
-# ICMP
-import os, sys, socket, struct, select, time
-ICMP_ECHO_REQUEST = 8
-def checksum(source_string):
-  sum = 0
-  countTo = (len(source_string)/2)*2
-  count = 0
-  while count<countTo:
-    thisVal = ord(source_string[count + 1])*256 + ord(source_string[count])
-    sum = sum + thisVal
-    sum = sum & 0xffffffff # Necessary?
-    count = count + 2
-  if countTo<len(source_string):
-    sum = sum + ord(source_string[len(source_string) - 1])
-    sum = sum & 0xffffffff # Necessary?
-  sum = (sum >> 16) + (sum & 0xffff)
-  sum = sum + (sum >> 16)
-  answer = ~sum
-  answer = answer & 0xffff
-  answer = answer >> 8 | (answer << 8 & 0xff00)
-  return answer
-def receive_one_ping(my_socket, ID, timeout):
-  timeLeft = timeout
-  while True:
-    startedSelect = time.time()
-    whatReady = select.select([my_socket], [], [], timeLeft)
-    howLongInSelect = (time.time() - startedSelect)
-    if whatReady[0] == []: # Timeout
-      return
-    timeReceived = time.time()
-    recPacket, addr = my_socket.recvfrom(1024)
-    icmpHeader = recPacket[20:28]
-    type, code, checksum, packetID, sequence = struct.unpack(
-      "bbHHh", icmpHeader
-    )
-    if packetID == ID:
-      bytesInDouble = struct.calcsize("d")
-      timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-      return timeReceived - timeSent
-    timeLeft = timeLeft - howLongInSelect
-    if timeLeft <= 0:
-      return
-def send_one_ping(my_socket, dest_addr, ID):
-  dest_addr = socket.gethostbyname(dest_addr)
-  my_checksum = 0
-  header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1) #压包
-  bytesInDouble = struct.calcsize("d")
-  data = (192 - bytesInDouble) * "Q"
-  data = struct.pack("d", time.time()) + data
-  my_checksum = checksum(header + data)
-  header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1)
-  packet = header + data
-  my_socket.sendto(packet, (dest_addr, 1)) # Don't know about the 1
-def do_one(dest_addr, timeout):
-  icmp = socket.getprotobyname("icmp")
-  try:
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-  except socket.error, (errno, msg):
-    if errno == 1:
-      msg = msg + (
-        " - Note that ICMP messages can only be sent from processes"
-        " running as root."
-      )
-      raise socket.error(msg)
-    raise # raise the original error
-  my_ID = os.getpid() & 0xFFFF
-  send_one_ping(my_socket, dest_addr, my_ID)
-  delay = receive_one_ping(my_socket, my_ID, timeout)
-  my_socket.close()
-  return delay
-def verbose_ping(dest_addr, timeout = 2, count = 100):
-  for i in xrange(count):
-    print "ping %s..." % dest_addr,
-    try:
-      delay = do_one(dest_addr, timeout)
-    except socket.gaierror, e:
-      print "failed. (socket error: '%s')" % e[1]
-      break
-    if delay == None:
-      print "failed. (timeout within %ssec.)" % timeout
-    else:
-      delay = delay * 1000
-      print "get ping in %0.4fms" % delay
+                    all_threads = []
+                    portIterator = (port for port in self.__tcpport_list)  # 用python的生成器保证不重复取完所有端口，也可以用Queue
+                    for _ in range(self.__thread_num):  # TCP扫描
+                        t = threading.Thread(target=self.tcpscan,
+                                             args=(ip, hostname, portIterator, result,))
+                        all_threads.append(t)
+                        t.start()
+                    t2 = threading.Thread(target=self.udpscan,args=(ip, hostname, result,))  # UDP扫描
+                    all_threads.append(t2)
+                    t2.start()
+                    for t in all_threads:
+                        t.join()
 
-def ping():
-    verbose_ping(ip,timeout=3,count=1)
+
+    def run(self):
+        # 多进程通信Queue
+        ipque = multiprocessing.Queue()
+        result = multiprocessing.Queue()
+        if isinstance(self.__ip_input, list) and self.__ip_input:
+            for ip in self.__ip_input:
+                if not ip in self.__black_ip:  # 如果ip输入合法且不属于黑名单库，添加进队列等待扫描
+                    ipque.put(ip)
+            all_process = []
+            for _ in range(self.__process_num):
+                p = multiprocessing.Process(target=self.scanip, args=(ipque, result))
+                p.start()
+                all_process.append(p)
+            while True:
+                for p in all_process[:]:
+                    if not p.is_alive():
+                        all_process.remove(p)
+                if not all_process:
+                    break
+            #  打印结果
+            while result.qsize():
+                value = result.get(True)
+                print value
+        else:
+            print "输入ip信息不合法，请检查{}".format(self.__ip_input)
+
+
+if __name__ == '__main__':
+    a = hostscan(ipinput='10.25.195.34-10.25.195.44',blackip=feature.blackip,tcpportlist=feature.tcpportlist, threadnum=40, process_num=20)
+    a.run()
